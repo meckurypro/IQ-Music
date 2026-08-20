772278840996cbbe26b5c38e665a12e81d184bbb
meckurypro <meckurypro@gmail.com>
2026-08-20 19:24:06 +0100

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,9 @@
 // src/components/Logo.jsx
 export default function Logo() {
-  return <img src="/iq-music-logo.png" alt="IQ Music" className="logo-mark" />
+  return (
+    <span className="logo-lockup">
+      <img src="/iq-music-logo.png" alt="IQ Music" className="logo-mark" />
+      <span className="logo-wordmark">IQ Music</span>
+    </span>
+  )
 }
